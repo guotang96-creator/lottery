ba0651ca013f21e5015973d84d1ceea91e990fd2
guotang96-creator <guotang96@gmail.com>
2026-04-01 15:50:50 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,34 +1,8 @@
-const CACHE_NAME = "jincai539-v40";
+const CACHE_NAME = "jincai539-v41";
 const ASSETS = [
   "./",
   "./index.html",
   "./style.css",
-  "./app.js?v=40",
-  "./manifest.json?v=40"
+  "./app.js?v=41",
+  "./manifest.json?v=41"
 ];
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
-});
-
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      )
-    )
-  );
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((cached) => cached || fetch(event.request))
-  );
-});
